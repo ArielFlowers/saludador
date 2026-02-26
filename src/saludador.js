159b02308c929e8435b7a19eb04dd50314f691fd
ArielFlowers <arielmakotoflores@gmail.com>
2026-02-26 18:51:21 -0400

    saluda con el nombre, genero, de acuerdo a la edad, en ingles y español, asi como de acuerdo a la hora que se tenga en el momento (ahora es noche)

--- src/saludador.js
+++ src/saludador.js
@@ -1,5 +1,9 @@
-function saludar(nombre,genero,edad) {
-    let saludo = "Hola";
+function saludar(nombre,genero,edad,idioma) {
+    let saludo;
+    const hora = new Date().getHours();
+    if (hora < 12) saludo = "Buenos días";
+    else if (hora < 18) saludo = "Buenas tardes";
+    else saludo = "Buenas noches";
 
     if (edad && edad > 30) {
         if (genero === "masculino") saludo += ", señor";
@@ -8,9 +12,22 @@ function saludar(nombre,genero,edad) {
         if (genero === "masculino") saludo += ", joven";
         else if (genero === "femenino") saludo += ", señorita";
     }
+
     if (nombre) {
         saludo += ` ${nombre}`;
     }
-    return saludo +"!";
+
+    if (idioma === "en") {
+        saludo = saludo.replace("Buenos días", "Good morning")
+                       .replace("Buenas tardes", "Good afternoon")
+                       .replace("Buenas noches", "Good evening")
+                       .replace("joven", "Mr.")
+                       .replace("señorita", "Miss.")
+                       .replace("señor", "Mr.")
+                       .replace("señora", "Mrs.");
+    }
+
+
+    return saludo + "!";
 }
 export default saludar;
--- src/saludador.test.js
+++ src/saludador.test.js
@@ -2,8 +2,12 @@ import saludar from "./saludador.js";
 
 describe("saludador", () => {
   it("deberia saludar", () => {
-    expect(saludar("pepe","masculino",70)).toEqual("Hola, señor pepe!");
-    expect(saludar("maria","femenino",12)).toEqual("Hola, señorita maria!");
+    expect(saludar("pepe","masculino",70,"es")).toEqual("Buenas noches, señor pepe!");
+    expect(saludar("maria","femenino",12,"en")).toEqual("Good evening, Miss. maria!");
+    expect(saludar("andres","masculino", 14,"en")).toEqual("Good evening, Mr. andres!");
+    expect(saludar("ximena","femenino", 44,"es")).toEqual("Buenas noches, señora ximena!");
+    expect(saludar("franz","masculino", 78,"en")).toEqual("Good evening, Mr. franz!");
+    expect(saludar("peter","masculino", 8,"es")).toEqual("Buenas noches, joven peter!");
 
   });
 });
